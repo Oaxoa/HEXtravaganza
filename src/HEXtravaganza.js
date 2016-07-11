be19f54d42a7b0228a659ed0478496220c3a8843
Pierluigi Pesenti <pierluigi.pesenti@appway.com>
2016-07-11 17:34:00 +0200

    Removed the logi to scan for matches in a text and added some tests

--- src/HEXtravaganza.js
+++ src/HEXtravaganza.js
@@ -1,8 +1,117 @@
 (function() {
 
 	var HEXtravaganza=(function() {
+		function parse(color) {
+			var r=/#([0-9a-fA-F]{1,8})/g;
 
+			if(!r.test(color)) {
+				var e=new Error('Bad argument format');
+				throw(e);
+				return false;
+			} else {
+				out=toTransformedColor(toColorCouple(color));
+				return out; 
+			}
 
+		}
+		function multiplyHEX(arg, n) {
+			n=n||2;
+			var out='', i, j, c;
+			for(i=0; i<arg.length; i++) {
+				c=arg.substr(i,1);
+				for(j=0; j<n; j++) {
+					out+=c;
+				}
+			}
+			return out;
+		}
+		function toColorCouple(arg) {
+			var color, alpha, alphaHEX;
+			var plainArg=arg.substr(1);
+			switch(plainArg.length) {
+				case 8:
+					alphaHEX=plainArg.substr(0,2);
+					color=plainArg.substr(2,6);
+					break;
+				case 7:
+					alphaHEX=multiplyHEX(plainArg.substr(0,1));
+					color=plainArg.substr(1,6);
+					break;
+				case 6:
+					alphaHEX='FF';
+					color=plainArg.substr(0,6);
+					break;
+				case 5:
+					alphaHEX=plainArg.substr(0,2);
+					color=plainArg.substr(2,3);
+					break;
+				case 4:
+					alphaHEX=multiplyHEX(plainArg.substr(0,1));
+					color=plainArg.substr(1,3);
+					break;
+				case 3:
+					alphaHEX='FF';
+					color=plainArg;
+					break;
+				case 2:
+					alphaHEX=multiplyHEX(plainArg.substr(0,1));
+					color=multiplyHEX(plainArg.substr(1,1),3);
+					break;
+				case 1:
+					alphaHEX='FF';
+					color=multiplyHEX(plainArg.substr(0,1),3);
+					break;
+			}
+			alpha=parseInt((parseInt(alphaHEX, 16)/255)*100)/100;
+			return {color:color, alpha:alpha};
+		}
+		function toTransformedColor(cc) {
+			if(cc.alpha===1) {
+				return '#'+cc.color;
+			} else {
+				return toRGBString(cc.color, cc.alpha);
+			}
+		}
+		function toRGBA(hex, alpha) {
+			var r,g,b,a,r1,b1,g1,a1;
+			switch(hex.length) {
+				case 3:
+					r1=hex.substr(0,1);
+					g1=hex.substr(1,1);
+					b1=hex.substr(2,1);
+
+					r=r1+r1;
+					g=g1+g1;
+					b=b1+b1;
+					break;
+				case 6:
+					r=hex.substr(0,2);
+					g=hex.substr(2,2);
+					b=hex.substr(4,2);
+					break;
+			}
+			r=parseInt(r, 16);
+			g=parseInt(g, 16);
+			b=parseInt(b, 16);
+			a=alpha;
+
+			var out={r:r,g:g,b:b,a:a};
+			return out;
+		}
+		function toRGBString(hex, alpha) {
+			var o=toRGBA(hex, alpha);
+			var out='rgba('+o.r+','+o.g+','+o.b+','+o.a+')';
+			return out;
+		}
+		function toNumber(arg) {
+			return parseInt('0x'+arg);
+		}
+
+		// PUBLIC API
+		
+		return {
+			parse:parse
+		}
 
 	})();
 	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
@@ -12,73 +121,4 @@
 	}
 })();
 
-function HEXtravaganza(arg) {
-		var r=/#([0-9a-fA-F]{1,8})/g, r2;
-
-		var hex, hexes=[], ends=[], res, out=arg;
-		while(res=r.exec(out)) {
-			hexes.push(res[1]);
-		}
-		for(j=0; j<hexes.length; j++) {
-			r=new RegExp('#('+hexes[j]+')([ ;])','g');
-			out=out.replace(r, toTransformedColor(toColorCouple(hexes[j]))+'$2');
-		}
-		return out;
-	}
-	function multiplyHEX(arg, n) {
-		n=n||2;
-		var out='', i, j, c;
-		for(i=0; i<arg.length; i++) {
-			c=arg.substr(i,1);
-			for(j=0; j<n; j++) {
-				out+=c;
-			}
-		}
-		return out;
-	}
-	function toColorCouple(arg) {
-		var color, alpha, alphaHEX;
-		switch(arg.length) {
-			case 8:
-				alphaHEX=arg.substr(0,2);
-				color=arg.substr(2,6);
-				break;
-			case 7:
-				alphaHEX=multiplyHEX(arg.substr(0,1));
-				color=arg.substr(1,6);
-				break;
-			case 6:
-				alphaHEX='FF';
-				color=arg.substr(0,6);
-				break;
-			case 5:
-				alphaHEX=arg.substr(0,2);
-				color=arg.substr(2,3);
-				break;
-			case 4:
-				alphaHEX=multiplyHEX(arg.substr(0,1));
-				color=arg.substr(1,3);
-				break;
-			case 3:
-				alphaHEX='FF';
-				color=arg;
-				break;
-			case 2:
-				alphaHEX=multiplyHEX(arg.substr(0,1));
-				color=multiplyHEX(arg.substr(1,1),3);
-				break;
-			case 1:
-				alphaHEX='FF';
-				color=multiplyHEX(arg.substr(0,1),3);
-				break;
-		}
-		alpha=parseInt((parseInt(alphaHEX, 16)/255)*100)/100;
-		return {color:color, alpha:alpha};
-	}
-	function toTransformedColor(cc) {
-		if(cc.alpha===1) {
-			return '#'+cc.color;
-		} else {
-			return toRGBString(cc.color, cc.alpha);
-		}
-	}
+	
--- test/HEXtravaganza.js
+++ test/HEXtravaganza.js
@@ -2,6 +2,57 @@ var test = require('tape');
 var HEXtravaganza=require('../src/HEXtravaganza.js');
 
 test('Library exists', function (t) {
-	t.plan(1);
 	t.equal(typeof HEXtravaganza, 'object');
+	t.end();
+});
+test('Check input format', function (t) {
+	t.throws(() => HEXtravaganza.parse('3'), Error, 'Bad argument format');
+	t.throws(() => HEXtravaganza.parse('Hello world'), Error, 'Bad argument format');
+	t.throws(() => HEXtravaganza.parse('#zzz'), Error, 'Bad argument format');
+	t.throws(() => HEXtravaganza.parse({}), Error, 'Bad argument format');
+	t.throws(() => HEXtravaganza.parse([]), Error, 'Bad argument format');
+	t.throws(() => HEXtravaganza.parse(0), Error, 'Bad argument format');
+	t.throws(() => HEXtravaganza.parse(1), Error, 'Bad argument format');
+	t.throws(() => HEXtravaganza.parse(true), Error, 'Bad argument format');
+	t.throws(() => HEXtravaganza.parse(false), Error, 'Bad argument format');
+	t.throws(() => HEXtravaganza.parse(undefined), Error, 'Bad argument format');
+	t.throws(() => HEXtravaganza.parse(), Error, 'Bad argument format');
+	t.throws(() => HEXtravaganza.parse(null), Error, 'Bad argument format');
+	t.throws(() => HEXtravaganza.parse(NaN), Error, 'Bad argument format');
+	t.end();
+});
+test('Color conversion', function (t) {
+	t.equal(HEXtravaganza.parse('#3'), '#333');
+	t.equal(HEXtravaganza.parse('#03'), 'rgba(51,51,51,0)');
+	t.equal(HEXtravaganza.parse('#13'), 'rgba(51,51,51,0.06)');
+	t.equal(HEXtravaganza.parse('#23'), 'rgba(51,51,51,0.13)');
+	t.equal(HEXtravaganza.parse('#F3'), '#333');
+	t.equal(HEXtravaganza.parse('#333'), '#333');
+	t.equal(HEXtravaganza.parse('#0333'), 'rgba(51,51,51,0)');
+	t.equal(HEXtravaganza.parse('#1333'), 'rgba(51,51,51,0.06)');
+	t.equal(HEXtravaganza.parse('#2333'), 'rgba(51,51,51,0.13)');
+	t.equal(HEXtravaganza.parse('#F333'), '#333');
+	t.equal(HEXtravaganza.parse('#00333'), 'rgba(51,51,51,0)');
+	t.equal(HEXtravaganza.parse('#01333'), 'rgba(51,51,51,0)');
+	t.equal(HEXtravaganza.parse('#03333'), 'rgba(51,51,51,0.01)');
+	t.equal(HEXtravaganza.parse('#06333'), 'rgba(51,51,51,0.02)');
+	t.equal(HEXtravaganza.parse('#10333'), 'rgba(51,51,51,0.06)');
+	t.equal(HEXtravaganza.parse('#80333'), 'rgba(51,51,51,0.5)');
+	t.equal(HEXtravaganza.parse('#FF333'), '#333');
+	t.equal(HEXtravaganza.parse('#333333'), '#333333');
+
+	t.equal(HEXtravaganza.parse('#0333333'), 'rgba(51,51,51,0)');
+	t.equal(HEXtravaganza.parse('#1333333'), 'rgba(51,51,51,0.06)');
+	t.equal(HEXtravaganza.parse('#2333333'), 'rgba(51,51,51,0.13)');
+	t.equal(HEXtravaganza.parse('#F333333'), '#333333');
+
+
+	t.equal(HEXtravaganza.parse('#00333333'), 'rgba(51,51,51,0)');
+	t.equal(HEXtravaganza.parse('#01333333'), 'rgba(51,51,51,0)');
+	t.equal(HEXtravaganza.parse('#03333333'), 'rgba(51,51,51,0.01)');
+	t.equal(HEXtravaganza.parse('#06333333'), 'rgba(51,51,51,0.02)');
+	t.equal(HEXtravaganza.parse('#10333333'), 'rgba(51,51,51,0.06)');
+	t.equal(HEXtravaganza.parse('#80333333'), 'rgba(51,51,51,0.5)');
+	t.equal(HEXtravaganza.parse('#FF333333'), '#333333');
+	t.end();
 });
